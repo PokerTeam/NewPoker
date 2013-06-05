8da5318e489fc7b9605d931083e29b964f03d8ad
denis kolpakov <kolpakovdenis21@gmail.com>
2013-06-05 23:53:35 +0300

    Add user tests 2

--- Tests_Kolpakov/tst_user_test.cpp
+++ Tests_Kolpakov/tst_user_test.cpp
@@ -3,6 +3,7 @@
 #include <QtCore/QCoreApplication>
 
 #include "../user.h"
+#include "../userinfo.h"
 
 class User_Test : public QObject
 {
@@ -15,6 +16,9 @@ private Q_SLOTS:
     void getUserName();
     void getUserId();
     void getMoney();
+    void isAllIn();
+    void isNotAllIn();
+    void putOnTable();
 };
 
 User_Test::User_Test()
@@ -37,6 +41,25 @@ void User_Test::getMoney()
 {
     User user = User(1, "TestName", "", 2);
     QCOMPARE(user.getMoney(), long(2));
+}
+
+void User_Test::isAllIn(){
+    UserInfo user = UserInfo("", 1, 100, 0);
+    user.putOnTable(100);
+    QCOMPARE(user.isAllIn(), true);
+}
+
+void User_Test::isNotAllIn(){
+    UserInfo user = UserInfo("", 1, 100, 0);
+    user.putOnTable(99);
+    QCOMPARE(user.isAllIn(), false);
+}
+
+void User_Test::putOnTable(){
+    UserInfo user = UserInfo("", 1, 100, 0);
+    user.putOnTable(60);
+    QCOMPARE(user.getUserMoneyOnTable(), long(60));
+    QCOMPARE(user.getUserMoney(), long(40));
 }
 
 QTEST_MAIN(User_Test)
